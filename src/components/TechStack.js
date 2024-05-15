143806a80da06ddc5a5d14d1ce9fe0606efdc502
SimBuds <casey-hsu@outlook.com>
2024-05-15 12:31:26 -0400

    Job Search v0.85

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import {
   siPython, siJavascript, siCsharp, siPhp, 
-  siSwift, siAndroid, siRust, siHtml5, siCss3,
-  siOracle, siMysql, siMongodb, siPostgresql,
-  siLinux, siApple, siWindows,
+  siAndroid, siOracle, siMysql, siMongodb,
+  siPostgresql, siLinux, siApple, siWindows,
   siDotnet, siSpringboot, siReact, siAngular,
   siDocker, siPostman, siGit
   
@@ -13,36 +12,32 @@ import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import '../assets/TechStack.css';
 
 const techCategories = {
-  "Programming Languages:": [
+  "Programming Languages": [
+    { name: 'JavaScript', color: '#F7DF1E', icon: siJavascript },
     { name: 'Java', color: '#f89820', icon: <FontAwesomeIcon icon={faCoffee} /> },
     { name: 'Python', color: '#3776AB', icon: siPython },
-    { name: 'JavaScript', color: '#F7DF1E', icon: siJavascript },
     { name: 'C#', color: '#239120', icon: siCsharp },
     { name: 'PHP', color: '#777BB4', icon: siPhp },
-    { name: 'Swift', color: '#FA7343', icon: siSwift },
-    { name: 'Android', color: '#3DDC84', icon: siAndroid },
-    { name: 'Rust', color: '#B7410E', icon: siRust },
-    { name: 'HTML', color: '#E34F26', icon: siHtml5 },
-    { name: 'CSS', color: '#1572B6', icon: siCss3 }
+    { name: 'Android', color: '#3DDC84', icon: siAndroid }
   ],
-  "Databases:": [
-    { name: 'Oracle SQL', color: '#F80000', icon: siOracle },
+  "Databases": [
+    { name: 'Oracle', color: '#F80000', icon: siOracle },
     { name: 'MySQL', color: '#4479A1', icon: siMysql },
     { name: 'MongoDB', color: '#47A248', icon: siMongodb },
     { name: 'Postgres', color: '#336791', icon: siPostgresql }
   ],
-  "Operating Systems:": [
+  "Operating Systems": [
     { name: 'Linux', color: '#FCC624', icon: siLinux },
     { name: 'Mac', color: '#A2AAAD', icon: siApple },
     { name: 'Windows', color: '#0078D6', icon: siWindows }
   ],
-  "Frameworks:": [
+  "Frameworks": [
     { name: 'ASP.NET', color: '#512BD4', icon: siDotnet },
-    { name: 'Spring Boot', color: '#6DB33F', icon: siSpringboot },
+    { name: 'Spring', color: '#6DB33F', icon: siSpringboot },
     { name: 'React', color: '#61DAFB', icon: siReact },
     { name: 'Angular', color: '#DD0031', icon: siAngular }
   ],
-  "Tools:": [
+  "Tools": [
     { name: 'Docker', color: '#2496ED', icon: siDocker },
     { name: 'Postman', color: '#FF6C37', icon: siPostman },
     { name: 'Git', color: '#F05032', icon: siGit }
@@ -52,13 +47,13 @@ const techCategories = {
 function TechStack() {
   return (
     <div className="tech-stack">
-      <div className="title">
-        <h2>My Tech Stack</h2>
-        {Object.entries(techCategories).map(([category, techs]) => (
+      <h2>My Tech Stack</h2>
+      <div className="category-row">
+        {['Operating Systems', 'Programming Languages', 'Tools'].map(category => (
           <div className="category" key={category}>
-            <h4>{category}</h4>
-            <div className={`${category === "Programming Languages:" ? 'languages-items' : 'category-items'}`}>
-              {techs.map(tech => (
+            <h3>{category}</h3>
+            <div className="tech-items">
+              {techCategories[category].map(tech => (
                 <div className="tech-item" key={tech.name} style={{ backgroundColor: tech.color }}>
                   {React.isValidElement(tech.icon) ? (
                     tech.icon
@@ -70,7 +65,26 @@ function TechStack() {
               ))}
             </div>
           </div>
-      ))}
+        ))}
+      </div>
+      <div className="category-row">
+        {['Databases', 'Frameworks'].map(category => (
+          <div className="category" key={category}>
+            <h3>{category}</h3>
+            <div className="tech-items">
+              {techCategories[category].map(tech => (
+                <div className="tech-item" key={tech.name} style={{ backgroundColor: tech.color }}>
+                  {React.isValidElement(tech.icon) ? (
+                    tech.icon
+                  ) : (
+                    <svg viewBox="0 0 24 24" dangerouslySetInnerHTML={{ __html: tech.icon.svg }} />
+                  )}
+                  <p>{tech.name}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
